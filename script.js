c64b604549f6b8f14363b299c5a7c4dea3adbad8
Nadaseperde <nadaseperde2024@gmail.com>
2024-08-21 10:40:00 -0300

    Site

---
+++ script.js
@@ -0,0 +1,10 @@
+function menuu() {
+      let menuMobile= document.querySelector('.mobile-menu');
+      if (menuMobile.classList.contains('open')) {
+          menuMobile.classList.remove('open');
+          document.querySelector('.icon').src= "imgs/menu.svg";
+      } else {
+          menuMobile.classList.add('open');
+          document.querySelector('.icon').src= "imgs/close.svg";
+      }
+    }
